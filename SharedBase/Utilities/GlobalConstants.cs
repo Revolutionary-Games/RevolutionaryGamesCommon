6eddfae1f438f498f6d8d1fd8f49d61e4cad3b85
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-08-11 19:14:19 +0300

    Added size constants to the base project

--- SharedBase/Utilities/GlobalConstants.cs
+++ SharedBase/Utilities/GlobalConstants.cs
@@ -7,4 +7,8 @@ public static class GlobalConstants
 {
     public const int MinEmailLength = 3;
     public const int MaxEmailLength = 250;
+
+    public const int KIBIBYTE = 1024;
+    public const int MEBIBYTE = KIBIBYTE * KIBIBYTE;
+    public const int GIBIBYTE = MEBIBYTE * 1024;
 }
