d48499ded5b23cd9e29a73ed35b21105b95e05e2
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-08-31 18:15:31 +0300

    Converted deploy script to C#

--- ScriptsBase/Utilities/ConsoleHelpers.cs
+++ ScriptsBase/Utilities/ConsoleHelpers.cs
@@ -2,10 +2,16 @@ namespace ScriptsBase.Utilities;
 
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 public static class ConsoleHelpers
 {
     public static void CleanConsoleStateForExit()
+    {
+        CleanConsoleStateForInput();
+    }
+
+    public static void CleanConsoleStateForInput()
     {
         Console.ResetColor();
     }
@@ -15,6 +21,39 @@ public static class ConsoleHelpers
         ColourConsole.WriteErrorLine(message);
         CleanConsoleStateForExit();
         Environment.Exit(1);
+    }
+
+    /// <summary>
+    ///   Waits until a new line of input is available
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token that should become canceled if CTRL-C is pressed</param>
+    /// <returns>True if execution should continue, false if cancellation was requested</returns>
+    /// <remarks>
+    ///   <para>
+    ///     The cancellation token can be setup for example with <see cref="CreateSimpleConsoleCancellationSource"/>
+    ///   </para>
+    /// </remarks>
+    public static async Task<bool> WaitForInputToContinue(CancellationToken cancellationToken)
+    {
+        CleanConsoleStateForInput();
+
+        var readKeyTask = new Task(() => _ = Console.ReadLine());
+
+        readKeyTask.Start();
+
+        try
+        {
+            await readKeyTask.WaitAsync(cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            return false;
+        }
+
+        if (cancellationToken.IsCancellationRequested)
+            return false;
+
+        return true;
     }
 
     public static CancellationTokenSource CreateSimpleConsoleCancellationSource()
