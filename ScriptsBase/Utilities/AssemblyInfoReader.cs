f83e634f27ca59babe39fd7deec0c478398028ba
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-09-04 21:36:07 +0300

    Ported the steam compile mode scripts, removed some already ported scripts

--- ScriptsBase/Utilities/AssemblyInfoReader.cs
+++ ScriptsBase/Utilities/AssemblyInfoReader.cs
@@ -10,9 +10,11 @@ public static class AssemblyInfoReader
     private static readonly Regex AssemblyVersionRegex = new(@"AssemblyVersion\(""([\d.]+)""\)");
     private static readonly Regex AssemblyExtraVersionRegex = new(@"AssemblyInformationalVersion\(""([^""]*)""\)");
 
-    public static string ReadVersionFromAssemblyInfo(string file = "Properties/AssemblyInfo.cs")
+    public static string ReadVersionFromAssemblyInfo(bool includeInformationalVersion = false,
+        string file = "Properties/AssemblyInfo.cs")
     {
         string? version = null;
+        string additionalVersion = string.Empty;
 
         foreach (var line in File.ReadLines(file, Encoding.UTF8))
         {
@@ -30,9 +32,11 @@ public static class AssemblyInfoReader
             {
                 if (match.Groups[1].Length > 0)
                 {
+                    additionalVersion = match.Groups[1].Value;
+
                     // TODO: not the cleanest to combine this syntax check here, but this is how it was in the ruby
                     // version
-                    if (!match.Groups[1].Value.StartsWith('-'))
+                    if (!additionalVersion.StartsWith('-'))
                         throw new Exception("AssemblyInformationalVersion must start with a dash (or be empty)");
                 }
             }
@@ -45,6 +49,11 @@ public static class AssemblyInfoReader
         if (!Version.TryParse(version, out _))
         {
             throw new Exception($"Invalid version format for string: {version}");
+        }
+
+        if (includeInformationalVersion)
+        {
+            return $"{version}{additionalVersion}";
         }
 
         return version;
