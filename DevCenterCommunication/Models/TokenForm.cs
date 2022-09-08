ce386d575e233e248d8b6c870f851ee8943afc3e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-09-08 11:13:45 +0300

    Moved TokenForm to the communications project

---
+++ DevCenterCommunication/Models/TokenForm.cs
@@ -0,0 +1,11 @@
+namespace ThriveDevCenter.Shared.Forms;
+
+using System.ComponentModel.DataAnnotations;
+using DevCenterCommunication;
+
+public class TokenForm
+{
+    [Required]
+    [MaxLength(CommunicationConstants.MAXIMUM_TOKEN_LENGTH)]
+    public string Token { get; set; } = string.Empty;
+}
