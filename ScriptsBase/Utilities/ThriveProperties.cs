b6fe2fd3f41f9aed850d8517db8124f6d4a2d080
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-09-10 13:06:54 +0300

    Moved the Thrive export folder name computation to the common project

---
+++ ScriptsBase/Utilities/ThriveProperties.cs
@@ -0,0 +1,41 @@
+﻿namespace ScriptsBase.Utilities;
+
+using System;
+using Models;
+
+/// <summary>
+///   Properties about Thrive that are needed by multiple project's scripts
+/// </summary>
+public static class ThriveProperties
+{
+    public static string GetFolderNameForPlatform(PackagePlatform platform, string thriveVersion, bool steamMode)
+    {
+        string suffix = string.Empty;
+
+        if (steamMode)
+        {
+            suffix = "_steam";
+        }
+
+        var platformName = GetBasePlatformPartOfFolderName(platform);
+
+        return $"Thrive_{thriveVersion}_{platformName}{suffix}";
+    }
+
+    public static string GetBasePlatformPartOfFolderName(PackagePlatform platform)
+    {
+        switch (platform)
+        {
+            case PackagePlatform.Linux:
+                return "linux_x11";
+            case PackagePlatform.Windows:
+                return "windows_desktop";
+            case PackagePlatform.Windows32:
+                return "windows_desktop_(32-bit)";
+            case PackagePlatform.Mac:
+                return "mac_osx";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(platform), platform, null);
+        }
+    }
+}
