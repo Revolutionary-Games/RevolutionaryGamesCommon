29f62e1a16b474c04ee2914941267bede7dd4e91
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-09-10 15:06:44 +0300

    Converted the prepare files script to C#

--- ScriptsBase/Utilities/CopyHelpers.cs
+++ ScriptsBase/Utilities/CopyHelpers.cs
@@ -1,5 +1,6 @@
 ﻿namespace ScriptsBase.Utilities;
 
+using System;
 using System.IO;
 
 public static class CopyHelpers
@@ -14,6 +15,43 @@ public static class CopyHelpers
     public static void CopyToFolder(string file, string folder, bool overwrite = true)
     {
         File.Copy(file, Path.Join(folder, Path.GetFileName(file)), overwrite);
+    }
+
+    /// <summary>
+    ///   Copies folders and files recursively while preserving symlinks
+    /// </summary>
+    /// <param name="fromFolder">Where to copy from</param>
+    /// <param name="targetFolder">Target folder to copy to</param>
+    /// <param name="overwrite">If true overwriting files happens silently</param>
+    public static void CopyFoldersRecursivelyWithSymlinks(string fromFolder, string targetFolder,
+        bool overwrite = false)
+    {
+        if (Directory.Exists(fromFolder))
+            throw new ArgumentException("copy from must be an existing folder", nameof(fromFolder));
+
+        Directory.CreateDirectory(targetFolder);
+
+        foreach (var file in Directory.GetFiles(fromFolder))
+        {
+            var info = new FileInfo(file);
+
+            var target = Path.Join(targetFolder, Path.GetFileName(file));
+
+            if (info.LinkTarget != null)
+            {
+                // Handle link copying
+                File.CreateSymbolicLink(target, info.LinkTarget);
+            }
+            else
+            {
+                info.CopyTo(target, overwrite);
+            }
+        }
+
+        foreach (var directory in Directory.GetDirectories(fromFolder))
+        {
+            CopyFoldersRecursivelyWithSymlinks(directory, Path.Join(targetFolder, Path.GetFileName(directory)));
+        }
     }
 
     public static void MoveToFolder(string file, string folder, bool overwrite = true)
