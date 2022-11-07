bb453f2ab85e61e142787b11e7d8e0e39190b84b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-07 18:04:33 +0200

    Added quality checks for DehydratedObjectIdentification

--- DevCenterCommunication/Models/DehydratedObjectIdentification.cs
+++ DevCenterCommunication/Models/DehydratedObjectIdentification.cs
@@ -16,8 +16,30 @@ public class DehydratedObjectIdentification
     [MaxLength(100)]
     public string Sha3 { get; set; }
 
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        if (obj.GetType() != GetType())
+            return false;
+
+        return Equals((DehydratedObjectIdentification)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Sha3.GetHashCode();
+    }
+
     public override string ToString()
     {
         return $"sha3:{Sha3}";
+    }
+
+    protected bool Equals(DehydratedObjectIdentification other)
+    {
+        return Sha3 == other.Sha3;
     }
 }
