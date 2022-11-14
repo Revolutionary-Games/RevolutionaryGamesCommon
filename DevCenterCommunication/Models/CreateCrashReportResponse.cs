717b8f091eb5283b7deb788dd49e408785da4e58
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-14 18:35:16 +0200

    Moved CreateCrashReportResponse to the common module

---
+++ DevCenterCommunication/Models/CreateCrashReportResponse.cs
@@ -0,0 +1,15 @@
+namespace DevCenterCommunication.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+public class CreateCrashReportResponse
+{
+    [Required]
+    [JsonPropertyName("createdId")]
+    public long CreatedId { get; set; }
+
+    [Required]
+    [JsonPropertyName("deleteKey")]
+    public string DeleteKey { get; set; } = string.Empty;
+}
