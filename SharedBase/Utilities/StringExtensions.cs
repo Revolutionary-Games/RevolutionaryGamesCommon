56413ae21f821fb951e394642dd3b65441c825dd
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-12-08 15:23:57 +0200

    Removed a bunch of TODOs about done things and adjusted some still relevant ones

--- SharedBase/Utilities/StringExtensions.cs
+++ SharedBase/Utilities/StringExtensions.cs
@@ -2,7 +2,8 @@ namespace SharedBase.Utilities;
 
 public static class StringExtensions
 {
-    // TODO: change this to the utf-8 truncate character
+    // TODO: change this to the utf-8 truncate character (though this might maybe need to be context specific
+    // or at least the launcher fonts needs to be checked to make sure that they have the right character)
     public const string TruncateText = "...";
 
     public static string Truncate(this string? str, int length = 30)
