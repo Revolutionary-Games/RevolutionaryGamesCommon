58c83db841e563672c4bf2335af4453c387fd05c
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-02-16 15:15:47 +0200

    Rewrite tool has now most of the class member sorting done
    still needs some rarer stuff and some method name sorting

--- ScriptsBase/Checks/RewriteTool.cs
+++ ScriptsBase/Checks/RewriteTool.cs
@@ -1,20 +1,69 @@
 ﻿namespace ScriptsBase.Checks;
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Utilities;
 
 /// <summary>
 ///   Uses our custom special logic to do some rewriting in
 /// </summary>
 public class RewriteTool : CodeCheck
 {
+    public const string NoModifierPlacementType = "no modifier";
+
+    /// <summary>
+    ///   Basic order of class members, see <see cref="Rewriter.MemberOrderComparer"/> for the full implementation
+    ///   of the rules
+    /// </summary>
+    public static readonly IReadOnlyCollection<string> ModifiersOrder = new[]
+    {
+        "public",
+        NoModifierPlacementType,
+        "internal",
+
+        // TODO: this might not work, this is technically an allowed modifier but we don't use this anywhere
+        "protected internal",
+        "protected",
+        "private",
+    };
+
+    public static readonly IReadOnlyCollection<SyntaxKind> SyntaxTypeOrder = new[]
+    {
+        SyntaxKind.FieldDeclaration,
+        SyntaxKind.ConstructorDeclaration,
+        SyntaxKind.DelegateDeclaration,
+        SyntaxKind.EventFieldDeclaration,
+        SyntaxKind.InterfaceDeclaration,
+        SyntaxKind.EnumDeclaration,
+        SyntaxKind.PropertyDeclaration,
+        SyntaxKind.ConversionOperatorDeclaration,
+        SyntaxKind.OperatorDeclaration,
+        SyntaxKind.IndexerDeclaration,
+        SyntaxKind.MethodDeclaration,
+        SyntaxKind.ClassDeclaration,
+        SyntaxKind.StructDeclaration,
+    };
+
+    public static readonly IReadOnlyCollection<Regex> OrderOfMethodNames = new[]
+    {
+        new Regex(@"^_Ready"),
+        new Regex(@"^ResolveNodeReferences"),
+        new Regex(@"^_EnterTree"),
+        new Regex(@"^_ExitTree"),
+        new Regex(@"^_.*"),
+    };
+
     private const string StartFileEnumerateFolder = "./";
 
     public override async Task Run(CodeCheckRun runData, CancellationToken cancellationToken)
@@ -148,6 +197,110 @@ public class RewriteTool : CodeCheck
             return base.VisitPropertyDeclaration(node);
         }
 
+        /// <summary>
+        ///   Handles checking that class members are in the correct order
+        /// </summary>
+        public override SyntaxNode? VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            // Because the SyntaxList builder modifies the node identities, we need to create this temporary list
+            // to be able to compare if the order changed or not
+            var newOrder = node.Members.Order(new MemberOrderComparer()).ToList();
+
+            // Skip any further processing if the order is the same
+            if (newOrder.SequenceEqual(node.Members))
+                return base.VisitClassDeclaration(node);
+
+            if (newOrder.Count != node.Members.Count)
+                throw new Exception("Member reordering somehow lost a member");
+
+            var reordered = new SyntaxList<MemberDeclarationSyntax>(newOrder);
+
+            runData.OutputTextWithMutex(
+                $"{GetLocation(node.Span)} has class \"{node.Identifier.ToString()}\" with members in incorrect order");
+
+            var expected = MemberDeclarationsToTextList(reordered);
+            var current = MemberDeclarationsToTextList(node.Members);
+
+            if (expected == current)
+            {
+                // This indicates a problem in the order comparison
+                runData.OutputWarningWithMutex("Expected and current order string representations are the same");
+            }
+
+            runData.OutputTextWithMutex($"Expected order:\n{expected}\nBut current order is:\n{current}");
+
+            runData.OutputTextWithMutex(
+                "Note: reordering may not properly move all related comments or pragmas, so please verify " +
+                "the reordering is fully correct before committing");
+
+            var newClass = SyntaxFactory.ClassDeclaration(node.AttributeLists, node.Modifiers, node.Keyword,
+                node.Identifier, node.TypeParameterList, node.BaseList, node.ConstraintClauses, node.OpenBraceToken,
+                reordered, node.CloseBraceToken, node.SemicolonToken);
+
+            return base.VisitClassDeclaration(newClass);
+        }
+
+        private static string MemberDeclarationsToTextList(IEnumerable<MemberDeclarationSyntax> members)
+        {
+            var stringBuilder = new StringBuilder();
+
+            bool first = true;
+            foreach (var memberDeclarationSyntax in members)
+            {
+                if (!first)
+                    stringBuilder.Append('\n');
+
+                first = false;
+
+                var name = GetNameFromMemberDeclaration(memberDeclarationSyntax);
+                if (name != null)
+                    stringBuilder.Append($" - {name}");
+            }
+
+            if (stringBuilder.Length < 1)
+                stringBuilder.Append(" - all members of unknown type to display names");
+
+            return stringBuilder.ToString();
+        }
+
+        private static string? GetNameFromMemberDeclaration(MemberDeclarationSyntax memberDeclarationSyntax)
+        {
+            switch (memberDeclarationSyntax)
+            {
+                case FieldDeclarationSyntax fieldSyntax:
+                    return fieldSyntax.Declaration.Variables.FirstOrDefault()?.Identifier.ToString() ??
+                        "unknown variable name";
+                case MethodDeclarationSyntax methodSyntax:
+                    return methodSyntax.Identifier.ToString();
+                case PropertyDeclarationSyntax propertySyntax:
+                    return propertySyntax.Identifier.ToString();
+                case ClassDeclarationSyntax classSyntax:
+                    return classSyntax.Identifier.ToString();
+                case StructDeclarationSyntax structSyntax:
+                    return structSyntax.Identifier.ToString();
+                case InterfaceDeclarationSyntax interfaceSyntax:
+                    return interfaceSyntax.Identifier.ToString();
+                case DelegateDeclarationSyntax delegateSyntax:
+                    return delegateSyntax.Identifier.ToString();
+                case ConstructorDeclarationSyntax constructorSyntax:
+                    return constructorSyntax.Identifier.ToString();
+                case EnumDeclarationSyntax enumDeclaration:
+                    return enumDeclaration.Identifier.ToString();
+                case EventFieldDeclarationSyntax eventFieldSyntax:
+                    return eventFieldSyntax.Declaration.Variables.FirstOrDefault()?.Identifier.ToString() ??
+                        "unknown event field name";
+                case ConversionOperatorDeclarationSyntax conversionOperatorSyntax:
+                    return $"conversion operator {conversionOperatorSyntax.Type}" +
+                        $"{conversionOperatorSyntax.ParameterList.ToString()}";
+                case OperatorDeclarationSyntax operatorSyntax:
+                    return $"operator {operatorSyntax.OperatorToken}{operatorSyntax.ParameterList.ToString()}";
+                case IndexerDeclarationSyntax indexerSyntax:
+                    return $"{indexerSyntax.ThisKeyword}{indexerSyntax.ParameterList.ToString()}";
+                default:
+                    return $"Unknown member of kind: {memberDeclarationSyntax.Kind()}";
+            }
+        }
+
         private string GetLocation(TextSpan span)
         {
             return $"Line {sourceText.Lines.GetLinePosition(span.Start).Line + 1}";
@@ -175,6 +328,104 @@ public class RewriteTool : CodeCheck
 
             return (newNode.WithLeadingTrivia(oldNode.GetLeadingTrivia()),
                 oldNode.WithLeadingTrivia(singleSpaceTrivia));
+        }
+
+        private class MemberOrderComparer : IComparer<MemberDeclarationSyntax>
+        {
+            public int Compare(MemberDeclarationSyntax? x, MemberDeclarationSyntax? y)
+            {
+                if (x == null || y == null)
+                    throw new ArgumentNullException();
+
+                if (x.Equals(y))
+                    return 0;
+
+                // Compare first based on the member type to get the rough sections down
+                int xKindIndex = SyntaxTypeOrder.IndexOf(x.Kind());
+                int yKindIndex = SyntaxTypeOrder.IndexOf(y.Kind());
+
+                if (xKindIndex != -1 && yKindIndex != -1)
+                {
+                    if (xKindIndex < yKindIndex)
+                        return -1;
+
+                    if (xKindIndex > yKindIndex)
+                        return 1;
+                }
+
+                // TODO: remove
+                if (xKindIndex == -1 || yKindIndex == -1)
+                    Debugger.Break();
+
+                // Compare access modifiers next
+                int xIndex;
+                if (x.Modifiers.Count > 0 && y.Modifiers.Count > 0)
+                {
+                    // TODO: special case for internal and protected at the same time
+                    xIndex = ModifiersOrder.IndexOf(x.Modifiers.Select(m => m.ToString())
+                        .FirstOrDefault(m => ModifiersOrder.Contains(m)));
+                }
+                else
+                {
+                    // No modifier is by default at this point
+                    xIndex = ModifiersOrder.IndexOf(NoModifierPlacementType);
+                }
+
+                int yIndex;
+                if (y.Modifiers.Count > 0)
+                {
+                    yIndex = ModifiersOrder.IndexOf(y.Modifiers.Select(m => m.ToString())
+                        .FirstOrDefault(m => ModifiersOrder.Contains(m)));
+                }
+                else
+                {
+                    yIndex = ModifiersOrder.IndexOf(NoModifierPlacementType);
+                }
+
+                if (xIndex < yIndex)
+                    return -1;
+
+                if (xIndex > yIndex)
+                    return 1;
+
+                // Compare other than access modifiers next
+                bool xConst = x.Modifiers.Any(m => m.IsKind(SyntaxKind.ConstKeyword));
+                bool yConst = y.Modifiers.Any(m => m.IsKind(SyntaxKind.ConstKeyword));
+
+                if (xConst && !yConst)
+                    return -1;
+
+                if (!xConst && yConst)
+                    return 1;
+
+                bool xStatic = x.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword));
+                bool yStatic = y.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword));
+
+                if (xStatic && !yStatic)
+                    return -1;
+
+                if (!xStatic && yStatic)
+                    return 1;
+
+                bool xReadOnly = x.Modifiers.Any(m => m.IsKind(SyntaxKind.ReadOnlyKeyword));
+                bool yReadOnly = y.Modifiers.Any(m => m.IsKind(SyntaxKind.ReadOnlyKeyword));
+
+                if (xReadOnly && !yReadOnly)
+                    return -1;
+
+                if (!xReadOnly && yReadOnly)
+                    return 1;
+
+                // Modifiers are all the same, compare some special names
+                if (x is MethodDeclarationSyntax xMethod && y is MethodDeclarationSyntax yMethod)
+                {
+                    // xMethod.Identifier.ToString()
+                    // yMethod.Identifier.ToString()
+                }
+
+                // Everything we check for is equal
+                return 0;
+            }
         }
     }
 }
