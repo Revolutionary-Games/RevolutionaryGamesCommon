4657ef7de68cb72fcf226a5bb04609501a627455
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-02-16 14:31:34 +0200

    Added a helper method for getting the index of an item in a sequence

---
+++ ScriptsBase/Utilities/LinqHelpers.cs
@@ -0,0 +1,30 @@
+﻿namespace ScriptsBase.Utilities;
+
+using System.Collections.Generic;
+
+public static class LinqHelpers
+{
+    /// <summary>
+    ///   Returns the index of an element in an enumerable
+    /// </summary>
+    /// <param name="enumerable">The sequence to search</param>
+    /// <param name="value">The value to search for</param>
+    /// <typeparam name="T">Type of the sequence</typeparam>
+    /// <returns>Index of the value, -1 when not found</returns>
+    public static int IndexOf<T>(this IEnumerable<T> enumerable, T value)
+    {
+        int index = 0;
+
+        foreach (var current in enumerable)
+        {
+            if (ReferenceEquals(current, value) || (!ReferenceEquals(current, null) && current.Equals(value)))
+            {
+                return index;
+            }
+
+            ++index;
+        }
+
+        return -1;
+    }
+}
