6eab995edfa391879a1a21de452cb03db03c4df5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-05-18 14:41:15 +0300

    Added utility to format bytes as GiB, switched MiB format to double
    to ensure that large numbers don't cause problems

--- SharedBase/Utilities/ValuePrintHelpers.cs
+++ SharedBase/Utilities/ValuePrintHelpers.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 
 public static class ValuePrintHelpers
 {
-    public static string BytesToMiB(this float number, int decimals = 2, bool suffix = true,
+    public static string BytesToMiB(this double number, int decimals = 2, bool suffix = true,
         bool alwaysShowDecimals = false)
     {
         var asMib = Math.Round(number / GlobalConstants.MEBIBYTE, decimals);
@@ -27,15 +27,49 @@ public static class ValuePrintHelpers
         return result + " MiB";
     }
 
+    public static string BytesToGiB(this double number, int decimals = 2, bool suffix = true,
+        bool alwaysShowDecimals = true)
+    {
+        var asMib = Math.Round(number / GlobalConstants.GIBIBYTE, decimals);
+
+        string result;
+
+        if (alwaysShowDecimals)
+        {
+            result = asMib.ToString($"N{decimals}", CultureInfo.CurrentCulture);
+        }
+        else
+        {
+            result = asMib.ToString(CultureInfo.CurrentCulture);
+        }
+
+        if (!suffix)
+            return result;
+
+        return result + " GiB";
+    }
+
     public static string BytesToMiB(this long number, int decimals = 2, bool suffix = true,
         bool alwaysShowDecimals = false)
     {
-        return ((float)number).BytesToMiB(decimals, suffix, alwaysShowDecimals);
+        return ((double)number).BytesToMiB(decimals, suffix, alwaysShowDecimals);
     }
 
     public static string BytesToMiB(this int number, int decimals = 2, bool suffix = true,
         bool alwaysShowDecimals = false)
     {
-        return ((float)number).BytesToMiB(decimals, suffix, alwaysShowDecimals);
+        return ((double)number).BytesToMiB(decimals, suffix, alwaysShowDecimals);
+    }
+
+    public static string BytesToGiB(this long number, int decimals = 2, bool suffix = true,
+        bool alwaysShowDecimals = true)
+    {
+        return ((double)number).BytesToGiB(decimals, suffix, alwaysShowDecimals);
+    }
+
+    public static string BytesToGiB(this int number, int decimals = 2, bool suffix = true,
+        bool alwaysShowDecimals = true)
+    {
+        return ((double)number).BytesToGiB(decimals, suffix, alwaysShowDecimals);
     }
 }
