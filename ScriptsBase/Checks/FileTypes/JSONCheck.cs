f7edaaf32c11d850e65c44815cb7489ce0246489
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-12-21 10:45:53 +0200

    Switch dotnet-tools.json to wanting a newline at the end

--- ScriptsBase/Checks/FileTypes/JSONCheck.cs
+++ ScriptsBase/Checks/FileTypes/JSONCheck.cs
@@ -3,13 +3,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 
 public class JSONCheck : FileCheck
 {
-    public JSONCheck() : base(".json")
+    public const string DotnetToolsConfigFileName = "dotnet-tools.json";
+
+    private readonly IReadOnlyCollection<string> fileTypesToNotHaveTrailingNewLine;
+
+    public JSONCheck(string[]? filesWithoutTrailingNewLine = null) : base(".json")
     {
+        fileTypesToNotHaveTrailingNewLine = filesWithoutTrailingNewLine ?? new[] { DotnetToolsConfigFileName };
     }
 
     public override async IAsyncEnumerable<string> Handle(string path)
@@ -32,10 +38,21 @@ public class JSONCheck : FileCheck
             parsed.WriteTo(jsonWriter);
         }
 
-        // Add a trailing newline if not already added
+        bool addTrailingNewLine = ShouldHaveNewLine(path);
+
         memoryStream.Seek(-1, SeekOrigin.End);
-        if (memoryStream.ReadByte() != '\n')
-            memoryStream.WriteByte((byte)'\n');
+
+        // Add a trailing newline if not already added
+        if (addTrailingNewLine)
+        {
+            if (memoryStream.ReadByte() != '\n')
+                memoryStream.WriteByte((byte)'\n');
+        }
+        else
+        {
+            if (memoryStream.ReadByte() == '\n')
+                memoryStream.SetLength(memoryStream.Length - 1);
+        }
 
         memoryStream.Position = 0;
 
@@ -43,11 +60,13 @@ public class JSONCheck : FileCheck
 
         var newContent = await reader.ReadToEndAsync();
 
-        if (!newContent.EndsWith("\n"))
-            throw new Exception("Generated JSON doesn't end with a newline (script problem)");
+        if (addTrailingNewLine)
+        {
+            if (!newContent.EndsWith("\n"))
+                throw new Exception("Generated JSON doesn't end with a newline (script problem)");
+        }
 
         // Trying to make this work nicely on Windows
-        // TODO: test on Windows
         if (OperatingSystem.IsWindows())
             newContent = newContent.Replace("\r\n", "\n");
 
@@ -58,5 +77,10 @@ public class JSONCheck : FileCheck
         await File.WriteAllTextAsync(path, newContent, Encoding.UTF8);
 
         yield return "JSON formatting made changes";
+    }
+
+    private bool ShouldHaveNewLine(string path)
+    {
+        return !fileTypesToNotHaveTrailingNewLine.Any(path.EndsWith);
     }
 }
