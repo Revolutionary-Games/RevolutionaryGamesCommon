1cec54c06ca4bc2ae2e9beeede25f2458becf6ee
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-04-22 16:02:38 +0300

    Created a new shared project to have launcher-thrive shared stuff

---
+++ LauncherThriveShared/ThriveLauncherSharedConstants.cs
@@ -0,0 +1,22 @@
+namespace LauncherThriveShared;
+
+/// <summary>
+///   Constants that are shared between the Thrive Launcher and Thrive itself
+/// </summary>
+public static class ThriveLauncherSharedConstants
+{
+    public const string DISABLE_VIDEOS_LAUNCH_OPTION = "--thrive-disable-videos";
+    public const string OPENED_THROUGH_LAUNCHER_OPTION = "--thrive-started-by-launcher";
+    public const string OPENING_LAUNCHER_IS_HIDDEN = "--thrive-launcher-hidden";
+    public const string THRIVE_LAUNCHER_STORE_PREFIX = "--thrive-store=";
+    public const string THRIVE_LAUNCH_ID_PREFIX = "--thrive-launch-id=";
+
+    public const string SKIP_CPU_CHECK_OPTION = "--skip-cpu-check";
+    public const string DISABLE_CPU_AVX_OPTION = "--disable-avx";
+
+    public const string STARTUP_SUCCEEDED_MESSAGE = "------------ Thrive Startup Succeeded ------------";
+    public const string USER_REQUESTED_QUIT = "User requested program exit, Thrive will close shortly";
+    public const string REQUEST_LAUNCHER_OPEN = "------------ SHOWING LAUNCHER REQUESTED ------------";
+
+    public const string LATEST_START_INFO_FILE_NAME = "latest_start.json";
+}
---
+++ SharedBase/Models/ThriveStartInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedBase.Models;
+
+using System;
+
+public class ThriveStartInfo
+{
+    public ThriveStartInfo(DateTime startedAt, string startId)
+    {
+        StartedAt = startedAt;
+        StartId = startId;
+    }
+
+    public DateTime StartedAt { get; }
+
+    /// <summary>
+    ///   Usually a GUID that identifies the start
+    /// </summary>
+    public string StartId { get; }
+}
