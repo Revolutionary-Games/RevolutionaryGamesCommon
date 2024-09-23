6b8112742509920b8660ef65369440fdd94a1d18
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-09-23 14:48:55 +0300

    Fix new checks parameter compile

--- Scripts/CodeChecks.cs
+++ Scripts/CodeChecks.cs
@@ -7,16 +7,17 @@ public class CodeChecks : CodeChecksBase<Program.CheckOptions>
 {
     public CodeChecks(Program.CheckOptions opts) : base(opts)
     {
+        ValidChecks = new Dictionary<string, CodeCheck>
+        {
+            { "files", new FileChecks() },
+            { "compile", new CompileCheck(!options.NoExtraRebuild) },
+            { "inspectcode", new InspectCode() },
+            { "cleanupcode", new CleanupCode() },
+            { "rewrite", new RewriteTool() },
+        };
     }
 
-    protected override Dictionary<string, CodeCheck> ValidChecks { get; } = new()
-    {
-        { "files", new FileChecks() },
-        { "compile", new CompileCheck() },
-        { "inspectcode", new InspectCode() },
-        { "cleanupcode", new CleanupCode() },
-        { "rewrite", new RewriteTool() },
-    };
+    protected override Dictionary<string, CodeCheck> ValidChecks { get; }
 
     protected override string MainSolutionFile => "RevolutionaryGamesCommon.sln";
 }
