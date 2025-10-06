c3eb578582e29a6425d08e4c60fc07bd8f52dc8b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-10-06 16:47:44 +0300

    Added IArchivable.cs

---
+++ SharedBase/Archive/IArchivable.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedBase.Archive;
+
+/// <summary>
+///   Interface for archivable objects
+/// </summary>
+public interface IArchivable
+{
+    public ushort CurrentArchiveVersion { get; }
+    public ArchiveObjectType ArchiveObjectType { get; }
+
+    public void WriteToArchive(ISArchiveWriter writer);
+}
